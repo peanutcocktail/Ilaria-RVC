6e443d83597f0d357614bf669e4d591f96de2d32
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-04 01:22:22 -0500

    update

--- install.js
+++ install.js
@@ -25,10 +25,8 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "app/env",                // Edit this to customize the venv folder path
         message: [
-          "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt"
         ]
       }
